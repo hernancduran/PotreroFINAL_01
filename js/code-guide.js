7d090545aaa1cbf1cdcc619bf9e03db8786f8b59
Hernán C. Durán <104223695+hernancduran@users.noreply.github.com>
2022-07-09 20:26:36 -0300

    Add files via upload

---
+++ js/code-guide.js
@@ -0,0 +1,21 @@
+function contactame() { 
+    alert ("221 556-4160 - hernancduran@gmail.com")
+}
+
+function fondooscuro() { 
+    document.body.style.backgroundColor = "black";
+    document.getElementById("btnsecsjs").style.backgroundColor = "white";
+    document.getElementById("btnsecsjs").style.borderColor = "white";
+    document.getElementById("titulocliente").style.color = "white";
+    document.getElementById("titulocliente1").style.color = "white";
+    document.getElementById("titulocliente2").style.color = "white";
+}  
+
+function fondoclaro() { 
+    document.body.style.backgroundColor = "white" ;
+    document.getElementById("btnsecsjs").style.backgroundColor = "transparent";
+    document.getElementById("btnsecsjs").style.borderColor = "transparent";
+    document.getElementById("titulocliente").style.color = "#0075FF";
+    document.getElementById("titulocliente1").style.color = "#0075FF";
+    document.getElementById("titulocliente2").style.color = "#0075FF"
+}
