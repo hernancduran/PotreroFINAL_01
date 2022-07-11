4519766e9491005fe7cda04df7529ec4eb890189
Hernán C. Durán <104223695+hernancduran@users.noreply.github.com>
2022-07-11 16:14:01 -0300

    Add files via upload

--- js/code-guide.js
+++ js/code-guide.js
@@ -11,11 +11,33 @@ function fondooscuro() {
     document.getElementById("titulocliente2").style.color = "white";
 }  
 
+function fondooscuro1() { 
+    document.body.style.backgroundColor = "black";
+    document.getElementById("btnsecsjs1").style.backgroundColor = "white";
+    document.getElementById("btnsecsjs1").style.borderColor = "white";
+    document.getElementById("titulolabel0").style.color = "white";
+    document.getElementById("NombreAyuda").style.color = "white";
+    document.getElementById("titulolabel1").style.color = "white";
+    document.getElementById("titulolabel2").style.color = "white";
+    document.getElementById("titulolabel3").style.color = "white";
+}  
+
 function fondoclaro() { 
     document.body.style.backgroundColor = "white" ;
     document.getElementById("btnsecsjs").style.backgroundColor = "transparent";
     document.getElementById("btnsecsjs").style.borderColor = "transparent";
     document.getElementById("titulocliente").style.color = "#0075FF";
     document.getElementById("titulocliente1").style.color = "#0075FF";
-    document.getElementById("titulocliente2").style.color = "#0075FF"
+    document.getElementById("titulocliente2").style.color = "#0075FF";
+}
+
+function fondoclaro1() { 
+    document.body.style.backgroundColor = "white" ;
+    document.getElementById("btnsecsjs1").style.backgroundColor = "transparent";
+    document.getElementById("btnsecsjs1").style.borderColor = "transparent";
+    document.getElementById("titulolabel0").style.color = "#212529";
+    document.getElementById("NombreAyuda").style.color = "#212529BF";
+    document.getElementById("titulolabel1").style.color = "#212529";
+    document.getElementById("titulolabel2").style.color = "#212529";
+    document.getElementById("titulolabel3").style.color = "#212529";
 }
